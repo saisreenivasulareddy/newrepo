30b34374e1921cebed7e2b0d0ecf45fbcfa80495
saisreenivasulareddy <saipesala123@gmail.com>
2024-11-01 17:54:13 +0530

    third commit

--- src/main/java/com/example/Test1/FirstConfig.java
+++ src/main/java/com/example/Test1/FirstConfig.java
@@ -2,8 +2,9 @@ package com.example.Test1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FirstConfig {
 	
 	@GetMapping("/")
